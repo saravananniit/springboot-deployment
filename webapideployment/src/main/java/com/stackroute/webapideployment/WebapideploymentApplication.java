0c63e0ff9b223c462a716a3aa9be4ee1d6c92e49
Saravanan.1.P <saravanan.1.p@niit.com>
2024-05-16 19:06:39 +0530

    source code given

---
+++ webapideployment/src/main/java/com/stackroute/webapideployment/WebapideploymentApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.webapideployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapideploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapideploymentApplication.class, args);
+	}
+
+}
---
+++ webapideployment/src/main/java/com/stackroute/webapideployment/controller/SpringController.java
@@ -0,0 +1,24 @@
+package com.stackroute.webapideployment.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Saravanan.1.P
+ * @Date 16-05-2024
+ */
+@RestController
+public class SpringController {
+
+    @GetMapping
+    public Object hello(){
+        Map<String, String> data =new HashMap<>();
+        data.put("name","saravanan");
+        data.put("email","saravanan.1.p@niit.com");
+        return data;
+    }
+}
